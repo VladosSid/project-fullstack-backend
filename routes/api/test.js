2b632281561398c007c81377d18e5e806d93c94e
VladosSid <sidorsv121097@gmail.com>
2023-03-28 18:01:29 +0300

    test

--- routes/api/test.js
+++ routes/api/test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/test", (req, res) => {
-  res.status(200).json({ message: "data" });
+  res.status(200).json({ message: "data test" });
 });
 
 module.exports = router;
