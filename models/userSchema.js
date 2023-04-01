252feda4de636d7dae883fbab5a8090f07a8b08f
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-01 18:14:48 +0300

    Update userSchema.js
    убираю необходимость вводить уникальные юзернеймы

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username is required'],
-        unique: true,
     },
     email: {
         type: String,
