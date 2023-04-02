02f0ccbed84ff22b0d880fe3341b11446487922e
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-02 22:33:08 +0300

    Update validateBody.js
    updated validation middleware > general error message

--- middleware/common/validateBody.js
+++ middleware/common/validateBody.js
@@ -4,8 +4,7 @@ const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      // next(HttpError(400, error.message));
-      next(HttpError(400, "Invalid email address format"));
+      next(HttpError(400, error.message));
     }
     next();
   };
