781038735dcdd6177004cd9e4b089c23a6f15009
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2023-04-09 17:40:42 +0300

    add motivation

--- controller/favoriteController/addToFavorite.js
+++ controller/favoriteController/addToFavorite.js
@@ -1,10 +1,16 @@
 const { addToFavoriteList } = require('../../service/favoriteService');
+const {
+  countFavouriteRecipes,
+} = require('../../middleware/modalMotivation/modalMotivation');
 
 const addToFavorite = async (req, res) => {
+  const message = await countFavouriteRecipes(req.user);
+  const motivationMessage = message ?? null;
   await addToFavoriteList(req);
   res.json({
     status: 'success',
     code: 200,
+    motivationMessage,
   });
 };
 
